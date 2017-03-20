96d085e46fb67a3726f3fb7ec721f71a4c3bc719
Chris Gray <cmg_business@hotmail.com>
2017-03-20 16:26:59 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log("Starting app.js...");
+
+const fs = require('fs');
+// const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+const argv = yargs.argv;
+
+let command = argv._[0];
+console.log('Command:', command);
+console.log('Yargs:', argv);
+
+if (command === 'add') {
+    let note = notes.addNote(argv.title, argv.body);
+    if(note) {
+        console.log("Note successfully added");
+        note.logNote(note);
+    } else {
+        console.log("Error with adding note");
+    }
+} else if(command === 'list') {
+    notes.getAll();
+} else if(command === 'read') {
+    let note = notes.getNote(argv.title);
+    if (note) {
+        console.log("Note Found");
+        note.logNote(note);
+    } else {
+        console.log("Note not found");
+    }
+} else if(command === 'remove') {
+    let noteRemoved = notes.removeNote(argv.title);
+    let message = noteRemoved ? "Note was removed" : "Note not found";
+    console.log(message);
+} else {
+    console.log('Command not recognised');
+}
